f46b6648ba538a8c09d3b65a9da3701ec56e44ef
tanico-kazuyo <kazuyo.rikudo@gmail.com>
2021-10-09 14:23:58 +0900

    merge  kuloko util

--- util/utils.py
+++ util/utils.py
@@ -1,4 +1,6 @@
 import joblib
+import os, socket
+
 def loadJbl(filepath):
     with open(filepath, mode="rb") as f:
         data = joblib.load(f)
@@ -8,3 +10,12 @@ def readJbl(data, filepath):
     with open(filepath, mode="wb") as f:
         joblib.dump(data, f)
     return 
+def get_env():
+    info = {}
+    info['hostname'] = socket.gethostname()
+    if info['hostname'] in ['Macico']:
+        env_name = 'DEV'
+    else:
+        env_name = None
+    info['env_name']=env_name
+    return info
