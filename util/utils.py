55662cd2168654cba30c499ad213feca67a8da03
tanico-kazuyo <kazuyo.rikudo@gmail.com>
2021-11-06 17:01:28 +0900

    add load functions

--- util/utils.py
+++ util/utils.py
@@ -1,5 +1,7 @@
 import joblib
 import os, socket
+import yaml
+import sys
 
 def loadJbl(filepath):
     """Loadd jbl files
@@ -18,6 +20,25 @@ def readJbl(data, filepath):
     with open(filepath, mode="wb") as f:
         joblib.dump(data, f)
     return 
+
+def load_config(path, default_path):
+    path = default_path if path is None else path
+    config = configparser.ConfigParser()
+    config.read(path, encoding='utf-8')
+    return config
+
+
+def load_yaml(path):
+    try:
+        with open('sample.yaml') as file:
+            obj = yaml.safe_load(file)
+        return obj
+    except Exception as e:
+        print('Exception occurred while loading YAML...', file=sys.stderr)
+        print(e, file=sys.stderr)
+        return None
+        
+
 def get_env():
     info = {}
     info['hostname'] = socket.gethostname()
@@ -33,5 +54,3 @@ def is_type(obj, _type):
         return True
     else:
         return False
-        
-    
