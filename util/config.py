7eda7ffcb4bf4afab4a89a3219624b033c887831
tanico-kazuyo <kazuyo.rikudo@gmail.com>
2021-11-14 12:26:36 +0900

    update config manager

--- util/config.py
+++ util/config.py
@@ -1,20 +1,30 @@
-
+import logging
+import os, sys
+import configparser
 class ConfigManager:
-    def __init__(config_dir):
+    def __init__(self,config_dir):
         self.config_dir = config_dir
         self.default_config_file = {
             "general":"config.ini",
             "model":"model_config.ini",
-            "private_api":"private_api.ini"
+            "private_api":"private_api.ini",
+            "mongo":"mongo_config.ini",
+            "log":"log_config.ini"
         }
         
     def load_ini_config(self, path=None,config_name=None,mode=None):
         config_ini = configparser.ConfigParser()
         path = os.path.join(self.config_dir, self.default_config_file[config_name] ) if path is None else path
-        return config_ini.read(path, encoding='utf-8')[mode]
+        config_ini.read(path, encoding='utf-8')
+        return config_ini if mode is None else config_ini[mode]
             
-    def load_dict_config(dict_obj=None,mode=None):
+    def load_dict_config(self,dict_obj=None,mode=None):
         config_ini = configparser.ConfigParser()
-        return parser.read_dict(dict_obj)[mode]
+        config_ini.read_dict(dict_obj)
+        return config_ini if mode is None else config_ini[mode]
+    
+    def load_log_config(self,log_path,log_name):
+        logging.config.fileConfig(os.path.join(self.config_dir ,self.default_config_file["log"]),defaults={'logfilename': log_path})
+        return logging.getLogger(log_name)
         
         
