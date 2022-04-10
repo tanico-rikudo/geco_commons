3fb4db4e8f400ec6762bff3f8108b535e4a0cafd
tanico-kazuyo <kazuyo.rikudo@gmail.com>
2022-04-10 10:25:19 +0900

    add YYYYYMMDDHHMM

--- util/daylib.py
+++ util/daylib.py
@@ -47,6 +47,10 @@ class daylib():
         return int(dt_obj.strftime('%Y%m%d'))
 
     @classmethod
+    def dt_to_strYMDHM(cls, dt_obj):
+        return dt_obj.strftime('%Y%m%d%H%M')
+
+    @classmethod
     def dt_to_strYMDHMSF(cls, dt_obj):
         return dt_obj.strftime('%Y%m%d%H%M%S%f')
     
