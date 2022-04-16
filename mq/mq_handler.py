f44328c6705076270cda78431f72661c5b58e3ec
tanico-kazuyo <kazuyo.rikudo@gmail.com>
2022-04-16 17:24:53 +0900

    Fix: mq setting

--- mq/mq_handler.py
+++ mq/mq_handler.py
@@ -94,26 +94,19 @@ class MqProvider(object):
 class RpcClient(object):
 
     def __init__(self, mqserver_host, mqname, routing_key, logger):
+        self.routing_key = routing_key
         self.mqserver_host = mqserver_host
         self.mqname = mqname
-        self.routing_key = routing_key
         self.logger = logger
-        # self.channel = None
         self.connect_mq()
-        self.connection = None
 
-        # TODO: initialize???
-        # result = self.channel.queue_declare(queue=self.mqname, exclusive=True)
         result = self.channel.queue_declare(queue=self.mqname, exclusive=False)
-
         self.callback_queue = result.method.queue
 
         self.channel.basic_consume(
             queue=self.callback_queue,
             on_message_callback=self.on_response,
             auto_ack=True)
-
-        self.logger.info(f"[DONE] RPC request client initilized. Host={mqserver_host}, Routing={routing_key}")
 
     def connect_mq(self):
         self.logger.info(
@@ -128,12 +121,11 @@ class RpcClient(object):
         try:
             if self.connection is None:
                 return False
-            return self.connection.is_open()
-        except Exception as e :
+            return self.connection.is_open
+        except Exception as e:
             self.logger.warning(
-                f"Connection Check Failure... Host={self.mqserver_host}:{e}")
+                f"Connection Check Failure... Host={self.mqserver_host}:{e}", exc_info=True)
             return False
-
 
     def close_mq(self):
         try:
@@ -153,7 +145,8 @@ class RpcClient(object):
         commannd_msg = "" if commannd_msg is None else commannd_msg
         self.response = None
         self.corr_id = str(uuid.uuid4())
-        self.logger.info(f"[REQUEST] Call RPC. ID={self.corr_id}, Command={commannd_msg}, Roiting={self.routing_key}")
+        self.logger.info(
+            f"[REQUEST] Call RPC. ID={self.corr_id}, Command={commannd_msg}, Roiting={self.routing_key}, CallBack to={self.callback_queue} ")
         self.channel.basic_publish(
             exchange='',
             routing_key=self.routing_key,
